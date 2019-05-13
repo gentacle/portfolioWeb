4cbc673f7769e450a8a671d8a611dcb6f4c4f43d
gentacle <gentacle00@gmail.com>
2019-05-13 23:39:37 +0900

    'update' : main text

--- src/script/mainPage.jsx
+++ src/script/mainPage.jsx
@@ -9,11 +9,12 @@ class MainPage extends Component {
   render(){
     return(
       <div>
+      <h1>gentacle 포트폴리오 페이지입니다.</h1>
       <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
+      <li>리액트로 만드는중</li>
+      <li>웹팩 적용해봄</li>
+      <li>배포 안되는거 이유를 모르겠네</li>
+      <li>npm 날리고 재설하니까 된다. 뭐냐</li>
       </ul>
 
       <button onClick={()=>(this.setState({value:this.state.value + 1}))}>{this.state.value}</button>
